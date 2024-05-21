8c61bb2f6186670ca58c955d78f4b5526399fe40
minhhoccode111 <minhhoccode111@gmail.com>
2024-05-21 09:54:03 +0700

    fix: packages

--- config/dbConnect.js
+++ config/dbConnect.js
@@ -1,11 +1,13 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.DATABASE_URI);
-    } catch (err) {
-        console.log(err);
-    }
-}
+  try {
+    mongoose.set("strictQuery", false);
+    await mongoose.connect(process.env.DATABASE_URI);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = connectDB;
+
